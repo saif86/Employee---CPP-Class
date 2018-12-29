8e76845095d5d9313e0a55827bf0638ab31cf9fb
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-29 17:46:13 +0800

    Update HourlyEmp.h

--- HourlyEmp.h
+++ HourlyEmp.h
@@ -1,18 +1,75 @@
-#ifndef HourlyEmp_HEADER 
-#define HourlyEmp_HEADER
+/**  HourlyEmp class inherited from Employee class.
+*
+* #include "HourlyEmp.h" <BR>
+* -llib
+*
+*/
+#ifndef HOURLY_EMP_H
+#define HOURLY_EMP_H
 
-#include "Employee.h" 
+// SYSTEM INCLUDES
+#include<iostream>
 
-// HourlyEmp class definition 
-class HourlyEmp :public Employee {
-private:
-	int hours;
-	double hourlyRate;
+// LOCAL INCLUDES
+#include "Employee.h"
+
+// HourlyEmp class definition
+class HourlyEmp : public Employee {
 public:
-	HourlyEmp(char* = "", int = 0, char* = "", double = 0.0, int =0, double = 0.0, Address = Address::getDefaultAddress());
-	void Print();
-	virtual double calcSalary();
+	// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	HourlyEmp(const string& = "", const string& = "", const Date& = Date::sGetDefaultDate(), const Date& = Date::sGetDefaultDate(), const Address& = Address::sGetDefaultAddress(), double = 0.0, double = 0.0, int = 0, double = 0.0);
 
-}; // end class HourlyEmp
+	// Use compiler-generated copy constructor, assignment, and destructor.
+	// HourlyEmp(const HourlyEmp&);
+	// HourlyEmp& operator=(const HourlyEmp&);
+	// ~HourlyEmp();
 
-#endif
+// OPERATIONS
+	/** Overriding function that prints all the details of the hourly employee.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void PrintEmployee()const;
+	/** Overriding function that calculates net salary
+	* of the HourlyEmp employee
+	*
+	* @param void
+	*
+	* @return net salary
+	*/
+	virtual double CalcSalary()const;
+
+// ACCESS
+	// setters
+	void SetHours(int = 0);
+	void SetHourlyRate(double = 0.0);
+	void SetHourlyEmp(const string& = "", const string& = "", const Address& = Address::sGetDefaultAddress(), double = 0.0, double = 0.0, int = 0, double = 0.0);
+	/**
+	# @overload void SetHourlyEmp(const Employee& aEmployee, int = 0, double = 0.0);
+	*/
+	void SetHourlyEmp(const Employee& aEmployee, int = 0, double = 0.0);
+	/**
+	# @overload void SetHourlyEmp(int = 0, double = 0.0);
+	*/
+	void SetHourlyEmp(int = 0, double = 0.0);
+	/**
+	# @overload void SetHourlyEmp(const HourlyEmp& aHourlyEmp);
+	*/
+	void SetHourlyEmp(const HourlyEmp& aHourlyEmp);
+
+	// getters
+	int GetHours()const;
+	double GetHourlyRate()const;
+	const HourlyEmp& GetHourlyEmp()const;
+private:
+// DATA MEMBERS
+	int mHours;
+	double mHourlyRate;
+};
+// end class HourlyEmp
+#endif  
+// _HOURLY_EMP_H_
