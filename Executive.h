4c910d1cb8f12d26c4bd794503c065070ef27a56
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-29 17:45:29 +0800

    Update Executive.h

--- Executive.h
+++ Executive.h
@@ -1,12 +1,55 @@
-#ifndef Executive_HEADER 
-#define Executive_HEADER
+/**  Executive class inherited from SalariedEmp class.
+*
+* #include "Executive.h" <BR>
+* -llib
+*
+*/
+#ifndef EXECUTIVE_H
+#define EXECUTIVE_H
 
-#include "Manager.h" 
+// SYSTEM INCLUDES
+#include<iostream>
 
-class Executive :public Manager { 
-public: 
-	Executive(char* = "", int = 0, char* = "", double = 0.0, double = 0.0, Address = Address::getDefaultAddress(), char* = "");
-	void Print(); 
+// LOCAL INCLUDES
+#include "Manager.h"
+
+// Executive class definition
+class Executive : public Manager {
+public:
+	// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Executive(const string& = "", const string& = "", const Date& = Date::sGetDefaultDate(), const Date& = Date::sGetDefaultDate(), const Address& = Address::sGetDefaultAddress(), double = 0.0, double = 0.0, const string& = "");
+
+	// Use compiler-generated copy constructor, assignment, and destructor.
+	// Executive(const Executive&);
+	// Executive& operator=(const Executive&);
+	// ~Executive();
+
+// OPERATIONS
+	/** Overriding function that prints all the details of the Executive.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void PrintEmployee()const;
+
+	// ACCESS
+	// setters
+	void SetExecutive(const string& = "", const string& = "", const Address& = Address::sGetDefaultAddress(), double = 0.0, double = 0.0, const string& = "");
+	/**
+	# @overload void SetExecutive(const Manager& aManager);
+	*/
+	void SetExecutive(const Manager& aManager);
+	/**
+	# @overload void SetExecutive(const Executive& aExecutive);
+	*/
+	void SetExecutive(const Executive& aExecutive);
+
+	// getters
+	const Executive& GetExecutive()const;
 };
-
-#endif
+// end class Executive
+#endif  
+// _EXECUTIVE_H_
