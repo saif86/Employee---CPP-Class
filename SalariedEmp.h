3dbd420d6c1fa49ba35bd708fe9fd6b426f846c9
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-29 17:47:42 +0800

    Update SalariedEmp.h

--- SalariedEmp.h
+++ SalariedEmp.h
@@ -1,15 +1,63 @@
-#ifndef SalariedEmp_HEADER 
-#define SalariedEmp_HEADER
+/**  SalariedEmp class inherited from Employee class.
+*
+* #include "SalariedEmp.h" <BR>
+* -llib
+*
+*/
+#ifndef SALARIED_EMP_H
+#define SALARIED_EMP_H
 
-#include "Employee.h" 
+// SYSTEM INCLUDES
+#include<iostream>
 
-// SalariedEmp class definition 
-class SalariedEmp :public Employee {
+// LOCAL INCLUDES
+#include "Employee.h"
+
+// SalariedEmp class definition
+class SalariedEmp : public Employee {
 public:
-	SalariedEmp(char* = "", int = 0, char* = "", double = 0.0, double = 0.0, Address = Address::getDefaultAddress());
-	void Print();
-	virtual double calcSalary();
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	SalariedEmp(const string& = "", const string& = "", const Date& = Date::sGetDefaultDate(), const Date& = Date::sGetDefaultDate(), const Address& = Address::sGetDefaultAddress(), double = 0.0, double = 0.0);
 
-}; // end class SalariedEmp
+	// Use compiler-generated copy constructor, assignment, and destructor.
+	// SalariedEmp(const SalariedEmp&);
+	// SalariedEmp& operator=(const SalariedEmp&);
+	// ~SalariedEmp();
 
-#endif
+// OPERATIONS
+	/** Overriding function that prints all the details of the Salaried employee.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void PrintEmployee()const;
+
+	/** Overriding function that calculates net salary 
+	* of the salaried employee
+	*
+	* @param void
+	*
+	* @return net salary
+	*/
+	virtual double CalcSalary()const;
+// ACCESS
+	// setters
+	void SetSalariedEmp(const string& = "", const string& = "", const Address& = Address::sGetDefaultAddress(), double = 0.0, double = 0.0);
+	/**
+	# @overload void SetSalariedEmp(const Employee& aEmployee);
+	*/
+	void SetSalariedEmp(const Employee& aEmployee);
+	/**
+	# @overload void SetSalariedEmp(const SalariedEmp& aSalariedEmp);
+	*/
+	void SetSalariedEmp(const SalariedEmp& aSalariedEmp);
+
+	// getters
+	const SalariedEmp& GetSalariedEmp()const;
+};
+// end class SalariedEmp
+#endif  
+// _SALARIED_EMP_H_
