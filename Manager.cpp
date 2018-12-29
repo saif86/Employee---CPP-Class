b86418bc88de3d9eba954503f062d0ac5ff993aa
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-29 17:46:32 +0800

    Update Manager.cpp

--- Manager.cpp
+++ Manager.cpp
@@ -1,22 +1,73 @@
-
-#include "Manager.h" 
-#include<iostream>
-
+#include "Manager.h"  // class implemented
+#include<string>
 using namespace std;
 
-Manager::Manager(char* aName, int aAge, char* aNIC, double aTaxRate, double aBasicSalary, Address add, char* aDepartment) :SalariedEmp(aName, aAge, aNIC, aTaxRate, aBasicSalary, add), Department(aDepartment) {
-	if (aDepartment != NULL) { 
-		int len = strlen(aDepartment) + 1; 
-		Department = new char[len]; 
-		strcpy_s(Department, len, aDepartment); 
-	} 
-	else { 
-		Department = new char; 
-		strcpy_s(Department, 1, "\0"); 
-	} 
-} 
+// File scope starts here 
 
-void Manager::Print() { 
-	SalariedEmp::Print();
-	cout << "Department: " << Department << endl; 
+/////////////////////////////// PUBLIC ///////////////////////////////////////
+
+//============================= LIFECYCLE ====================================
+
+// Manager Default + Overloaded Constructor
+Manager::Manager(const string& aName, const string& aNIC, const Date& aDateOfBirth, const Date& aDateOfHire, const Address& aHomeAddress, double aTaxRate, double aBasicSalary, const string& aDepartment) : SalariedEmp(aName, aNIC, aDateOfBirth, aDateOfHire, aHomeAddress, aTaxRate, aBasicSalary), mDepartment(aDepartment) {
+	// base class initilzation using member initlizer list
+	this->SetManager(0,aDepartment);
 }
+// end Manager constructor
+
+
+//============================= OPERATIONS ===================================
+// Overriding function that prints all the details of the Manager.
+void Manager::PrintEmployee()const {
+	SalariedEmp::PrintEmployee();
+	cout << "Department: " << this->GetDepartment() << endl;
+}
+// end function PrintEmployee
+
+
+//============================= ACESS      ===================================
+
+// function that sets department of Manager
+void Manager::SetDepartment(const string& aDepartment) {
+	this->mDepartment = aDepartment;
+}
+// end function SetDepartment
+
+// function that sets the Manager
+void Manager::SetManager(const string& aName, const string& aNIC, const Address& aHomeAddress, double aTaxRate, double aBasicSalary, const string& aDepartment) {
+	this->SetSalariedEmp(aName, aNIC, aHomeAddress, aTaxRate, aBasicSalary);
+	this->SetManager(0,aDepartment);
+}
+// end function SetManager
+
+// overloaded function that sets the Manager
+void Manager::SetManager(const SalariedEmp& aSalariedEmp, const string& aDepartment) {
+	this->SetSalariedEmp(aSalariedEmp.GetSalariedEmp());
+	this->SetManager(0,aDepartment);
+}
+// end function SetManager
+
+// overloaded function that sets the Manager
+void Manager::SetManager(int aDummy, const string& aDepartment) {
+	this->SetDepartment(aDepartment);
+}
+// end function SetManager
+
+// overloaded function that sets the Manager
+void Manager::SetManager(const Manager& aManager) {
+	this->SetManager(aManager.GetSalariedEmp(), aManager.GetDepartment());
+}
+// end function SetManager
+
+// function that gets department of Manager
+const string& Manager::GetDepartment()const {
+	return this->mDepartment;
+}
+// end function GetSales
+
+
+// function that gets the Manager
+const Manager& Manager::GetManager()const {
+	return *this;
+}
+// end function GetManager
